decbbbc3dbe740f9a312a708dcf0e35671ddd0f4
l0st15 <daniilhramov970@gmail.com>
2025-03-02 22:22:12 +0700

    ADD DBmanager.cpp, include_directories в CMakeLists.txt

---
+++ cpp/DBManager.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Даниил on 02.03.2025.
+//
+#include "DBManager.h"
+#include <iostream>
+
+// переписать методы используя параметизированые запросы и и sqlite3_stmt!!!
+
+
+
+
+
+
--- header/DBManager.h
+++ header/DBManager.h
@@ -5,8 +5,27 @@
 #ifndef APP_SERVER_DBMANAGER_H
 #define APP_SERVER_DBMANAGER_H
 
+#include "sqlite3.h"
+#include "string"
+#include "vector"
+#include "map"
+
 class DBManager
 {
+protected:
+    /*поля класса*/
+    sqlite3* db = nullptr; // указатель на БД
+    std::string path_to_db; // содержит путь до БД
+    std::string path_to_logfile; // путь до файла с логами
+    /*методы класса*/
+    DBManager();
+    DBManager(std::string& in_path_db, std::string& in_path_logfile); // конструктор по умолчанию(путь к БД прочитается из файла)
+    ~DBManager(); // деструктор класса
+    void open_connection(); // подлючение к базе данных
+    bool check_connection(); // проверка подключения к БД
+    void insert(sqlite3_stmt &in_stmt); // функция для выполнения запроса INSERT
+    std::vector<std::map<std::string, std::string>> select(sqlite3_stmt &in_stmt); // функция для выполнения запросов SELECT
+    virtual bool auth(std::string in_login, std::string in_hash_passwd) = 0; // вирутальная функция для аутентификации
 
 };
 
