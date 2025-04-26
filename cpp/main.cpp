981f8e8f49736cdc670efa8e715468952341b51e
l0st15 <daniilhramov970@gmail.com>
2025-04-26 20:57:34 +0700

    ADD класс Crypto

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "sodium.h"
+#include "Crypto.h"
 
 int main()
 {
@@ -8,8 +9,18 @@ int main()
         /* panic! the library couldn't be initialized; it is not safe to use */
         std::cout << "World";
     }
-    std::string a;
-    std::cin >> a;
+     Crypto test;
+
+    std::string test_uuid = test.uuidGen();
+    std::cout << "uuid: " << test_uuid << "\n";
+    std::string test_paswd = "12345678";
+    std::string hash = test.hashPassword(test_paswd);
+    std::cout << "Password: " << test_paswd << "\n" << "Hash: " << hash << "\n";
+    std::cout << "Verify: " << test.verifyPassword(hash, test_paswd) << "\n";
+    std::cout << "Valide uuid: " << test.isValidUuid(test_uuid);
+
+
+
     return 0;
 }
 
