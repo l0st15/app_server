e0e0851fcdcdb100c8e26bb3eceefb3e5a556580
Mikhail <babumikhail@rambler.ru>
2025-05-02 11:53:45 +0700

    особый коммит

--- cpp/network.cpp
+++ cpp/network.cpp
@@ -100,6 +100,7 @@ int Network::sendResponse(Response &res)
     http_response += std::to_string(res.statusCode);
     http_response += "" + res.statusMessage + "\r\n";
     http_response += "Content-Type: application/json\r\n";
+    http_response += "headerForSasha: U+1F4A9\r\n";
     http_response += "Content-Length: " + std::to_string(res.body_length);
     http_response += "Connection: close\r\n\r\n";
     http_response += res.body;
