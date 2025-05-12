742c6907c58fb9b24c1161bac024a1fe369a090d
l0st15 <daniilhramov970@gmail.com>
2025-05-12 20:19:54 +0700

    DEL Ненужный мусор

--- header/UserHandler.h
+++ header/UserHandler.h
@@ -4,12 +4,12 @@
 
 #ifndef APP_SERVER_USERHANDLER_H
 #define APP_SERVER_USERHANDLER_H
+
 #include "IRequestHandler.h"
 #include "Units.h"
 #include "sqlite3.h"
 #include "json.hpp"
 #include "Crypto.h"
-#include "DBmanager.h"
 
 class UserHandler : public IRequestHandler
 {
@@ -18,7 +18,6 @@ public:
     int userAuth(const std::string& uuid); //проверка uuid
 private:
     Crypto crypto_module;
-    DBmanager db_module;
     Response userReg(const Request& req); // регистрация пользователя
     Response userLogin(const Request& req); // вход в аккаунт
     Response userGetInfo(const Request& req); //получени данных из бд
