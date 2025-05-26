3ddb97f805261d8280b7eb0699a5a4a2536f21e2
l0st15 <daniilhramov970@gmail.com>
2025-05-26 08:42:05 +0700

    ADD Logger для ведения лог файлов

---
+++ header/Logger.h
@@ -0,0 +1,47 @@
+//
+// Created by Даниил on 26.05.2025.
+//
+
+#ifndef APP_SERVER_LOGGER_H
+#define APP_SERVER_LOGGER_H
+
+#include <json.hpp>
+#include <fstream>
+#include <chrono>
+#include <iomanip>
+#include <thread>
+
+class Logger {
+public:
+    Logger(const std::string& filename) : log_file(filename, std::ios::app) {}
+
+    void log(const std::string& type, const std::string& mes) {
+        nlohmann::json log_entry;
+        std::string thread_id_str = std::to_string(
+                std::hash<std::thread::id>{}(std::this_thread::get_id())
+        );
+        log_entry.push_back({"timestamp", currentTime()});
+        log_entry.push_back({"type", type});
+        log_entry.push_back({"message", mes});
+        log_entry.push_back({"thread_id", thread_id_str});
+
+        log_file << log_entry.dump(4) << "\n";
+    }
+
+    ~Logger() {
+        log_file.close();
+    }
+private:
+    std::string currentTime() {
+        auto now = std::chrono::system_clock::now();
+        auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+        return ss.str();
+    }
+    std::ofstream log_file;
+};
+
+
+#endif //APP_SERVER_LOGGER_H
